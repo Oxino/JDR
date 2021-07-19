679a6ea543d3c7210492b600c7b438783f80221e
DESKTOP-DH48608\Lucas <lucas-gil@hotmail.fr>
2021-07-19 14:24:05 +0200

    page inventory

---
+++ app/src/main/java/com/example/jdrandroidjava/ItemAdapter.java
@@ -0,0 +1,40 @@
+package com.example.jdrandroidjava;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+
+public class ItemAdapter extends ListAdapter<Item, ItemViewHolder> {
+
+    public ItemAdapter(@NonNull DiffUtil.ItemCallback<Item> diffCallback) {
+        super(diffCallback);
+    }
+
+    @Override
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return ItemViewHolder.create(parent);
+    }
+
+    @Override
+    public void onBindViewHolder(ItemViewHolder holder, int position) {
+        Item current = getItem(position);
+        holder.bind(current);
+    }
+
+    static class WordDiff extends DiffUtil.ItemCallback<Item> {
+
+        @Override
+        public boolean areItemsTheSame(@NonNull Item oldItem, @NonNull Item newItem) {
+            return oldItem == newItem;
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Item oldItem, @NonNull Item newItem) {
+            return oldItem.getName().equals(newItem.getName());
+        }
+    }
+}
